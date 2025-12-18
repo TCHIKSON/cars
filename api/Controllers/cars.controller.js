d58bab1ca273009847f803c35e9401016189ef80
Kevin Davy TCHIKAYA NDOMBI <kevin.tchikaya@yahoo.com>
2025-12-18 10:40:23 +0100

    3 comit

--- api/Controllers/cars.controller.js
+++ api/Controllers/cars.controller.js
@@ -6,4 +6,4 @@ app.get('/api/posts/:id', (req, res) => {
     const post = posts.find(p => p.id === parseInt(req.params.id));
     if (!post) return res.status(404).send("Voiture non trouvé");
     res.json(post);
-});
+})
